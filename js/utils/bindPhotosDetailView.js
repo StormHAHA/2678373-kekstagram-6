3339e19fdb0cf327ae0e497f96af95e9af5306ac
Emil Ibragimov <stormelit905@gmail.com>
2025-12-10 14:30:28 +0700

    make bound/undbound logic for listeners

--- js/utils/bindPhotosDetailView.js
+++ js/utils/bindPhotosDetailView.js
@@ -1,16 +1,33 @@
 import { showDetailView } from '../photosRender/photoDetailView.js';
 
-function bindPhotoDetailView(picturesContainer, photosData) {
-  const photosMap = new Map(photosData.map((photoData) => [photoData.id, photoData]));
+let isBound = false;
+let photosMap = null;
+let picturesContainer = null;
 
-  picturesContainer.addEventListener('click', (evt) => {
-    const picture = evt.target.closest('.picture');
-
-    const id = Number(picture.dataset.id);
-    const photoData = photosMap.get(id);
-
-    showDetailView(photoData);
-  });
+function bound(container, data) {
+  if (isBound) {
+    return;
+  }
+  picturesContainer = container;
+  photosMap = new Map(data.map((photoData) => [photoData.id, photoData]));
+  picturesContainer.addEventListener('click', onPhotoClick);
+  isBound = true;
 }
 
-export { bindPhotoDetailView };
+function unbound() {
+  if (!isBound) {
+    return;
+  }
+  picturesContainer.removeEventListener('click', onPhotoClick);
+  isBound = false;
+}
+
+function onPhotoClick(evt) {
+  const picture = evt.target.closest('.picture');
+
+  const id = Number(picture.dataset.id);
+  const photoData = photosMap.get(id);
+  showDetailView(photoData);
+}
+
+export { bound as bindPhotoDetailView, unbound as unbindPhotosDetailView };
